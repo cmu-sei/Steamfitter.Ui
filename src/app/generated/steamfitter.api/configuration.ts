0691e605419fd471c7d3db8f8a63e18b54fb6d55
Adam R. Welle <arwelle@cert.org>
2023-04-05 16:03:39 -0400

    syntax update

--- src/app/generated/steamfitter.api/configuration.ts
+++ src/app/generated/steamfitter.api/configuration.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Carnegie Mellon University. All Rights Reserved. 
+Copyright 2021 Carnegie Mellon University. All Rights Reserved.
  Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 */
 
@@ -58,8 +58,7 @@ export class Configuration {
         this.encoder = configurationParameters.encoder;
         if (configurationParameters.credentials) {
             this.credentials = configurationParameters.credentials;
-        }
-        else {
+        } else {
             this.credentials = {};
         }
 
